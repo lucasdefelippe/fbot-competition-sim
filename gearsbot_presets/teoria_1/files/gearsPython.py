f057ba37b9fa30f5ab89146c7ac01d3670ad71db
Lucas de Felippe <34761206+lucasdefelippe@users.noreply.github.com>
2022-07-08 00:52:34 -0300

    Delete gearsbot_presets/teoria_1 directory

--- gearsbot_presets/teoria_1/files/gearsPython.py
+++
@@ -1,8 +0,0 @@
-from init import * # Import all functions
-
-# Load Modules
-import tube_detection
-
-
-
-tube_detection.init_module()
--- gearsbot_presets/teoria_1/files/tube_detection.py
+++
@@ -1,6 +0,0 @@
-from init import *
-
-def init_module():
-    while True:
-        print(ultrasonic_lateral_lower_sensor.distance_centimeters)
-        time.sleep(0.2)
